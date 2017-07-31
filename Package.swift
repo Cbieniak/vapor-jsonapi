97e0e7ae48fd2c051c146880e536dba8106984a1
Christian Bieniak <cbieniak@gmail.com>
2017-07-31 22:24:16 +1000

    Update to vapor 2

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         Target(name: "VaporJsonApi")
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
     ],
     exclude: []
 )
