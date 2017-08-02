ef5fdb94b6cadc9e2e49fdd57a82404f52e07613
Christian Bieniak <cbieniak@gmail.com>
2017-08-02 21:38:20 +1000

    Add fluent provider to the package

--- Package.swift
+++ Package.swift
@@ -2,12 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "VaporJsonApi",
-    targets: [
-        Target(name: "VaporJsonApi")
-    ],
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2)
+        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1)
     ],
     exclude: []
 )
